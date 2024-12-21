bca8d33e72383f2455d4ab949f19494e2c8ac006
Nersesyan <davit.nersesyan@student.vinci.be>
2024-12-21 17:15:34 +0100

    end of day (pb partout)

--- public/js/script.js
+++ public/js/script.js
@@ -21,22 +21,24 @@ class Block {
     }
 }
 // Recup des données du DOM
-const gameContainer = document.querySelector('#game-container');
+const gameContainer = document.querySelector("#game-container");
 const tabObj = [];
 // Const et Let
+let tabRandom = [];
+let isMoving = false;
 // Fonctions
 function generateGrid() {
     for (const ligne of tabObj) {
-        const row = document.createElement('div');
-        row.className = 'row';
+        const row = document.createElement("div");
+        row.className = "row";
         for (const block of ligne) {
-            const carre = document.createElement('div');
+            const carre = document.createElement("div");
             if (block.isVide) {
-                carre.className = 'blockVide';
+                carre.className = "blockVide";
             }
             else {
-                carre.className = 'block';
-                const p = document.createElement('p');
+                carre.className = "block";
+                const p = document.createElement("p");
                 p.textContent = String(block.containsNumber);
                 carre.append(p);
             }
@@ -51,62 +53,88 @@ function generateTabObj() {
         for (let j = 0; j < 4; j++) {
             const block = new Block(true, 0);
             tabObj[i].push(block);
+            tabRandom.push([i, j]);
         }
     }
+}
+function blockAleatoire() {
+    const rand = Math.floor(Math.random() * tabRandom.length);
+    const nbRand = Math.floor(Math.random() * 2);
+    tabObj[tabRandom[rand][0]][tabRandom[rand][1]].isVide = false;
+    tabObj[tabRandom[rand][0]][tabRandom[rand][1]].containsNumber = nbRand == 0 ? 2 : 4;
+    tabRandom = tabRandom.filter(item => item[0] !== tabRandom[rand][0] || item[1] !== tabRandom[rand][1]);
 }
 function modifierTab(row, block, contains, isVide) {
     tabObj[row][block].isVide = isVide ?? true;
     tabObj[row][block].containsNumber = contains ?? 0;
 }
 function deplacer(callback, y, x) {
-    gameContainer.innerHTML = '';
+    gameContainer.innerHTML = "";
     let recommencer = 1;
     while (recommencer > 0) {
         recommencer = 0;
         for (let row = 0; row < 4; row++) {
             for (let block = 0; block < 4; block++) {
-                if (callback(row, block)) {
+                if (row + y >= 0 &&
+                    row + y < 4 &&
+                    block + x >= 0 &&
+                    block + x < 4 &&
+                    tabObj[row + y][block + x].containsNumber == tabObj[row][block].containsNumber
+                    && !tabObj[row][block].isVide) {
+                    tabObj[row + y][block + x].containsNumber = tabObj[row + y][block + x].containsNumber * 2;
+                    tabObj[row][block].isVide = true;
+                    tabRandom.push([row, block]);
+                    tabObj[row][block].containsNumber = 0;
+                    recommencer++;
+                    console.log("PLUS");
+                }
+                else if (callback(row, block)) {
                     tabObj[row + y][block + x].containsNumber = tabObj[row][block].containsNumber;
                     tabObj[row + y][block + x].isVide = false;
                     tabObj[row][block].isVide = true;
+                    tabRandom.push([row, block]);
                     tabObj[row][block].containsNumber = 0;
                     recommencer++;
+                    console.log("DEPLACEMENT", tabObj[row][block], [row, block], tabObj[row + y][block + x], [row + y, block + x]);
                 }
             }
         }
     }
+    blockAleatoire();
     generateGrid();
 }
 function dirBas(row, block) {
-    return row < 3 && tabObj[row + 1][block].isVide && !(tabObj[row][block].isVide);
+    return (row < 3 && tabObj[row + 1][block].isVide && !tabObj[row][block].isVide);
 }
 function dirHaut(row, block) {
-    return row > 0 && tabObj[row - 1][block].isVide && !(tabObj[row][block].isVide);
+    return (row > 0 && tabObj[row - 1][block].isVide && !tabObj[row][block].isVide);
 }
 function dirGauche(row, block) {
-    return block > 0 && tabObj[row][block - 1].isVide && !(tabObj[row][block].isVide);
+    return (block > 0 && tabObj[row][block - 1].isVide && !tabObj[row][block].isVide);
 }
 function dirDroit(row, block) {
-    return block < 3 && tabObj[row][block + 1].isVide && !(tabObj[row][block].isVide);
+    return (block < 3 && tabObj[row][block + 1].isVide && !tabObj[row][block].isVide);
 }
 function whichKey(event) {
-    if (event.key === 'ArrowUp') {
+    if (isMoving)
+        return;
+    isMoving = true;
+    if (event.key === "ArrowUp") {
         deplacer(dirHaut, -1, 0);
     }
-    else if (event.key === 'ArrowDown') {
+    else if (event.key === "ArrowDown") {
         deplacer(dirBas, 1, 0);
     }
-    else if (event.key === 'ArrowLeft') {
+    else if (event.key === "ArrowLeft") {
         deplacer(dirGauche, 0, -1);
     }
-    else if (event.key === 'ArrowRight') {
+    else if (event.key === "ArrowRight") {
         deplacer(dirDroit, 0, 1);
     }
+    isMoving = false;
 }
 generateTabObj();
-modifierTab(0, 2, 2, false);
-modifierTab(1, 3, 2, false);
-modifierTab(2, 1, 2, false);
-modifierTab(2, 2, 2, false);
+blockAleatoire();
+blockAleatoire();
 generateGrid();
-window.addEventListener('keyup', whichKey);
+window.addEventListener("keyup", whichKey);
--- src/script.ts
+++ src/script.ts
@@ -24,24 +24,27 @@ class Block {
 }
 
 // Recup des données du DOM
-const gameContainer: HTMLDivElement = document.querySelector('#game-container') as HTMLDivElement;
+const gameContainer: HTMLDivElement = document.querySelector(
+    "#game-container"
+) as HTMLDivElement;
 const tabObj: Block[][] = [];
 
 // Const et Let
+let tabRandom: number[][] = [];
+let isMoving = false;
 
 // Fonctions
 function generateGrid() {
     for (const ligne of tabObj) {
-        const row = document.createElement('div');
-        row.className = 'row';
+        const row = document.createElement("div");
+        row.className = "row";
         for (const block of ligne) {
-            const carre = document.createElement('div');
+            const carre = document.createElement("div");
             if (block.isVide) {
-                carre.className = 'blockVide';
-            }
-            else {
-                carre.className = 'block';
-                const p = document.createElement('p');
+                carre.className = "blockVide";
+            } else {
+                carre.className = "block";
+                const p = document.createElement("p");
                 p.textContent = String(block.containsNumber);
                 carre.append(p);
             }
@@ -57,71 +60,106 @@ function generateTabObj() {
         for (let j = 0; j < 4; j++) {
             const block: Block = new Block(true, 0);
             tabObj[i].push(block);
+            tabRandom.push([i, j]);
         }
     }
 }
 
-function modifierTab(row: number, block: number, contains?: number, isVide?: boolean): void {
+function blockAleatoire() {
+    const rand: number = Math.floor(Math.random() * tabRandom.length);
+    const nbRand: number = Math.floor(Math.random() * 2);
+
+    tabObj[tabRandom[rand][0]][tabRandom[rand][1]].isVide = false;
+    tabObj[tabRandom[rand][0]][tabRandom[rand][1]].containsNumber = nbRand == 0 ? 2 : 4;
+    tabRandom = tabRandom.filter(item => item[0] !== tabRandom[rand][0] || item[1] !== tabRandom[rand][1]);
+}
+
+function modifierTab(row: number,block: number,contains?: number,isVide?: boolean): void {
     tabObj[row][block].isVide = isVide ?? true;
-    tabObj[row][block].containsNumber = contains ?? 0
+    tabObj[row][block].containsNumber = contains ?? 0;
 }
 
 function deplacer(callback: (row: number, block: number) => boolean, y: number, x: number): void {
-    gameContainer.innerHTML = '';
+    gameContainer.innerHTML = "";
     let recommencer: number = 1;
     while (recommencer > 0) {
         recommencer = 0;
         for (let row = 0; row < 4; row++) {
             for (let block = 0; block < 4; block++) {
-                if (callback(row, block)) {
+                if (
+                    row + y >= 0 &&
+                    row + y < 4 &&
+                    block + x >= 0 &&
+                    block + x < 4 &&
+                    tabObj[row + y][block + x].containsNumber == tabObj[row][block].containsNumber 
+                    && !tabObj[row][block].isVide
+                ) {
+                    tabObj[row + y][block + x].containsNumber = tabObj[row + y][block + x].containsNumber * 2;
+                    tabObj[row][block].isVide = true;
+                    tabRandom.push([row, block]);
+                    tabObj[row][block].containsNumber = 0;
+                    recommencer++;
+                    console.log("PLUS");
+                } else if (callback(row, block)) {
                     tabObj[row + y][block + x].containsNumber = tabObj[row][block].containsNumber;
                     tabObj[row + y][block + x].isVide = false;
                     tabObj[row][block].isVide = true;
+                    tabRandom.push([row, block]);
                     tabObj[row][block].containsNumber = 0;
                     recommencer++;
+                    console.log("DEPLACEMENT", tabObj[row][block], [row, block], tabObj[row + y][block + x], [row+y, block+x]);
                 }
             }
         }
     }
+    blockAleatoire();
     generateGrid();
 }
 
 function dirBas(row: number, block: number): boolean {
-    return row < 3 && tabObj[row + 1][block].isVide && !(tabObj[row][block].isVide);
+    return (
+        row < 3 && tabObj[row + 1][block].isVide && !tabObj[row][block].isVide
+    );
 }
 
 function dirHaut(row: number, block: number): boolean {
-    return row > 0 && tabObj[row - 1][block].isVide && !(tabObj[row][block].isVide);
+    return (
+        row > 0 && tabObj[row - 1][block].isVide && !tabObj[row][block].isVide
+    );
 }
 
 function dirGauche(row: number, block: number): boolean {
-    return block > 0 && tabObj[row][block - 1].isVide && !(tabObj[row][block].isVide);
+    return (
+        block > 0 && tabObj[row][block - 1].isVide && !tabObj[row][block].isVide
+    );
 }
 
 function dirDroit(row: number, block: number): boolean {
-    return block < 3 && tabObj[row][block + 1].isVide && !(tabObj[row][block].isVide);
+    return (
+        block < 3 && tabObj[row][block + 1].isVide && !tabObj[row][block].isVide
+    );
 }
 
 function whichKey(event: KeyboardEvent): void {
-    if (event.key === 'ArrowUp') {
+    if (isMoving) return;
+    isMoving = true;
+
+    if (event.key === "ArrowUp") {
         deplacer(dirHaut, -1, 0);
-    }
-    else if (event.key === 'ArrowDown') {
+    } else if (event.key === "ArrowDown") {
         deplacer(dirBas, 1, 0);
-    }
-    else if (event.key === 'ArrowLeft') {
+    } else if (event.key === "ArrowLeft") {
         deplacer(dirGauche, 0, -1);
-    }
-    else if (event.key === 'ArrowRight') {
+    } else if (event.key === "ArrowRight") {
         deplacer(dirDroit, 0, 1);
     }
+
+    isMoving = false;
 }
 
 generateTabObj();
-modifierTab(0, 2, 2, false)
-modifierTab(1, 3, 2, false)
-modifierTab(2, 1, 2, false)
-modifierTab(2, 2, 2, false)
+blockAleatoire();
+blockAleatoire();
 generateGrid();
 
-window.addEventListener('keyup', whichKey);
+window.addEventListener("keyup", whichKey);
